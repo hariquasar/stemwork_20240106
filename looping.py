4680c9fc2f9e52dc16c8ec047eff1d7abcb7741c
hariquasar <138948301+hariquasar@users.noreply.github.com>
2024-01-06 12:24:17 +0800

    Create looping.py

---
+++ looping.py
@@ -0,0 +1,50 @@
+#while loop=重複
+# i = 1
+#while 條件:
+#   如果條件是真，做xx
+# while i <= 10:
+#     print(i)
+#     i = i + 1
+
+# count = 5
+# for i in range(10): #range(10) = 0, 1, 2, 3, 4...9
+#     print(i+1)
+
+# for i in range(88,100):
+#     print(i)
+
+# sum = 0
+# for i in range(1,11):
+#     sum = sum + i
+# print(sum)
+
+# a = 1
+# l = 10
+# sum = (a+l)*l/2
+# print(sum)
+
+# for i in range(0,20,3):
+#     print(i)
+
+# sum = 0
+# count = 0
+# myList = [12,74,70,20,100,50,80]
+# for i in myList:
+#     sum = sum + i
+# print(sum/len(myList))
+
+# for i in range(-10,0):
+#     print(i)
+
+# for i in range(5):
+#     print(i)
+# else:
+#     print("Done")
+
+#0 1 2 3 4 Done
+
+start = 25
+end = 50
+# prime number
+
+#用for loop 去搵全部質數
